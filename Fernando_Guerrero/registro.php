3aeb77c7e9fd80991fa15b94e9c0d8397df690bc
Fernando <fguerod123@gmail.com>
2019-11-01 19:44:58 +0100

    prueba

---
+++ Fernando_Guerrero/registro.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+   <p>   <?php echo 'El nombre es ' . $_POST [ 'nombre' ];  ?></p>
+
+   <p>   <?php echo 'El apellido es ' . $_POST [ 'apellidos' ];  ?></p>
+
+   <p>   <?php echo 'La edad es ' . $_POST [ 'edad' ];  ?></p>
+
+   <p>   <?php echo 'El la dirección es ' . $_POST [ 'direccion' ];  ?></p>
+
+</body>
+</html>
---
+++ prueba.php
@@ -0,0 +1,8 @@
+<HTML>
+ <H1>Probando Apache y PHP</H1>
+ Salida del comando phpinfo:
+
+ <?php phpinfo();
+ ?> 
+ 
+</HTML>
